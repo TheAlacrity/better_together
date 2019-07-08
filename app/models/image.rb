d05253db0f5e62822bf54ca2f4f33e24334b2388
TheAlacrity <brinker.jeff@gmail.com>
2019-07-08 16:42:09 -0500

    fixed logic of default image in image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,7 +7,7 @@ class Image < ApplicationRecord
   def default_image_url
     if self.file.attachment
       "http://localhost:3000" + rails_blob_path(self.file, only_path: true) 
-    elsif image_url == nil || image_url == ""
+    elsif image_url != nil || image_url != ""
       image_url
     else
       "https://i1.wp.com/crimsonems.org/wp-content/uploads/2017/10/profile-placeholder.gif?fit=250%2C250&ssl=1"
